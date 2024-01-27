726985a0ab16eb506373d2e3e3d30244bc0fc3b4
DRH <dragonfourtyseven@gmail.com>
2024-01-28 01:15:06 +0500

    missing commit

---
+++ prisma/migrations/20240127162208_changing_due_date_to_string_from_datetime/migration.sql
@@ -0,0 +1,22 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "framework" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'OPEN',
+    "priority" TEXT NOT NULL DEFAULT 'NORMAL',
+    "owner" TEXT DEFAULT 'Selected User',
+    "dueDate" TEXT NOT NULL,
+    "timeline" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    "budget" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Project" ("budget", "createdAt", "description", "dueDate", "framework", "id", "name", "owner", "priority", "status", "timeline", "updatedAt") SELECT "budget", "createdAt", "description", "dueDate", "framework", "id", "name", "owner", "priority", "status", "timeline", "updatedAt" FROM "Project";
+DROP TABLE "Project";
+ALTER TABLE "new_Project" RENAME TO "Project";
+CREATE UNIQUE INDEX "Project_id_key" ON "Project"("id");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
