1e0167902bbf5eeccc3f258f8cc60f723c48f74b
DRH <dragonfourtyseven@gmail.com>
2024-03-10 18:18:40 +0500

    feat: Adding Section Two to landing page

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -5,15 +5,15 @@ import {AspectRatio} from "@/components/ui/aspect-ratio";
 
 const LandingPage = () => {
     return (
-        <div className="w-full sm:-m-5 -m-3 bg-gradient-to-r from-secondary to-background">
+        <div className="w-full sm:-m-5 -m-3 py-10 bg-gradient-to-r from-secondary to-background">
             <Container>
-                <div className="flex p-10 justify-center flex-col sm:flex-row gap-3 sm:gap-5 text-sm items-center">
+                <div
+                    className="flex sm:p-5 p-3 justify-center flex-col sm:flex-row gap-3 sm:gap-5 text-sm items-center">
                     <div className="flex items-start justify-center flex-col gap-3 sm:gap-5">
                         <h1 className="sm:text-5xl text-3xl font-bold text-primary">Manage Your Projects Easily and Make
                             Amazing Experience.</h1>
-                        <p className="sm:text-lg text-sm mt-5 s text-primary">
-                            Project Manager is an open-source project management software. Project Manager is an
-                            invaluable open-source
+                        <p className="sm:text-lg text-sm mt-5 s text-primary">Project Manager is an invaluable
+                            open-source
                             solution designed for software houses and organizations seeking efficient workflow
                             management.
                             Tailored to streamline operations, it offers comprehensive tools to oversee projects, tasks,
